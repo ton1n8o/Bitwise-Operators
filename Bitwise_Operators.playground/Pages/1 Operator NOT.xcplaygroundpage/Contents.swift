efa352587e3aab955248b56ab0457780cf10031a
Antônio Carlos <antoniocarlos.dev@gmail.com>
2020-08-24 09:01:45 +0200

    Added examples for the NOT operator

--- Bitwise_Operators.playground/Pages/1 Operator NOT.xcplaygroundpage/Contents.swift
+++ Bitwise_Operators.playground/Pages/1 Operator NOT.xcplaygroundpage/Contents.swift
@@ -1,16 +1,29 @@
 //: [Overview - Table of Contents](0%20Overview)
 import Foundation
 
+// Some binary numbers
 var a = Int8(bitPattern: 0b0011_1100)
-var b = Int8(bitPattern: 0b0011_1100)
+var b = Int8(bitPattern: 0b0011_1000)
 var c = Int8(bitPattern: 0b1010_1011)
-var d = Int8(bitPattern: 0b1000_0000)
-var f = Int8(bitPattern: 0b0000_1000)
 
+// applying the NOT operator
 ~a
 ~b
 ~c
 
-String(a & b, radix: 2)
+/*
+ The operator NOT can be used to calculate the ones' complement of a binary number,
+ as it will invert all the bits in the binary representation of the number
+ (swapping 0s and 1s).
+
+ For instance, let's take 5 which is 0000_0101 in binary and apply the NOT operator.
+ We then have 1111_1010 which is -6.
+ */
+
+let num5 = Int8(bitPattern: 0b0000_0101)
+print(~num5)
+
+let num3 = Int8(bitPattern: 0b1111_1101) // -3
+print(~num3) // 2
 
 //: [Next - the AND operator](@next)
